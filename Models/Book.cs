19c03c4304a9b9e2a7c9413c0eae8d3e69f2693e
MARIA-ALEXANDRA VALEA <maria.valea@stud.ubbcluj.ro>
2021-12-08 14:16:32 +0200

    lab9

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,22 +8,27 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Valea_Maria_Alexandra_Lab8.Models
 {
     public class Book
-    {
-        public int ID { get; set; }
+        {
+            public int ID { get; set; }
 
-        [Display(Name = "Book Title")]
+            [Required, StringLength(150, MinimumLength = 3)]
+            [Display(Name = "Book Title")]
+            public string Title { get; set; }
 
-        public string Title { get; set; }
-        public string Author { get; set; }
+            [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma'Prenume Nume'"), Required, StringLength(50, MinimumLength = 3)]
+            public string Author { get; set; }
 
-        [Column(TypeName = "decimal(6, 2)")]
-        public decimal Price { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime PublishingDate { get; set; }
-        public int PublisherID { get; set; }
-        public Publisher Publisher { get; set; }
-        public ICollection<BookCategory> BookCategories { get; set; }
+            [Column(TypeName = "decimal(6, 2)")]
+
+            [Range(1, 300)]
+            public decimal Price { get; set; }
+
+            [DataType(DataType.Date)]
+            public DateTime PublishingDate { get; set; }
+
+            public int PublisherID { get; set; }
+            public Publisher Publisher { get; set; }
+            public ICollection<BookCategory> BookCategories { get; set; }
+
+        }
     }
-
-    } 
-
